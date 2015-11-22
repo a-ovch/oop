5aa9b21201d7604559fc9b31f6ed2b342fca00b9
Andrey Ovchinnikov <andrey.ps2007@gmail.com>
2015-11-22 23:46:40 +0300

    invert: реализовано открытие входного файла с информацией о матрице

--- lab1/invert/invert.cpp
+++ lab1/invert/invert.cpp
@@ -3,9 +3,28 @@
 
 #include "stdafx.h"
 
+#include <iostream>
+#include <fstream>
 
-int main()
+using namespace std;
+
+int main(int argc, char* argv[])
 {
+	if (argc != 2)
+	{
+		cout << "Invalid arguments number!" << endl
+			 << "Usage:" << endl
+			 << "\tinvert.exe <matrix file>" << endl;
+		return 1;
+	}
+
+	ifstream inputFile(argv[1]);
+	if (!inputFile.is_open())
+	{
+		cout << "Error! Can't open specified file \"" << argv[1] << "\"." << endl;
+		return 1;
+	}
+
     return 0;
 }
 
